d387c9eb6d3db0853a2fd3964774f56ff5979802
Jake Bayer <jakebayerct@gmail.com>
2022-01-29 10:09:14 -0500

    Update (see notes)
    - Added prisoner placeholder materials
    - Set up random spawning code
    - Set up placeholder prefabs for player, tree, cage, and prisoner
    - Created a scene for prefab creation
    - Added Resources folder
    - Added Prisoner tag

---
+++ 2022 Global Game Jam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<GameManager>();
+            return instance;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPrefabs();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void SpawnPrefabs()
+    {
+        SpawnPoint[] findSpawnPoints = FindObjectsOfType<SpawnPoint>();
+        for(int i = 0; i < findSpawnPoints.Length; i++)
+        {
+            int canSpawn = Random.Range(0, 2);
+            switch(findSpawnPoints[i].typeToSpawn)
+            {
+                case SpawnPoint.SpawnType.Player:
+                    SpawnPlayer(findSpawnPoints[i].transform.position);
+                    break;
+                case SpawnPoint.SpawnType.Tree:
+                    if(canSpawn == 1)
+                    {
+                        SpawnTree(findSpawnPoints[i].transform.position);
+                    }
+                    break;
+                case SpawnPoint.SpawnType.Cage:
+                    if (canSpawn == 1)
+                    {
+                        SpawnCage(findSpawnPoints[i].transform.position);
+                        SpawnPrisoner(findSpawnPoints[i].transform.position);
+                    }
+                    break;
+            }
+        }
+    }
+    private void SpawnPlayer(Vector3 spawnPosition)
+    {
+        GameObject go = Instantiate((GameObject)Resources.Load("Prefabs/Player"));
+        go.transform.position = new Vector3(spawnPosition.x, spawnPosition.y, spawnPosition.z);
+    }
+    private void SpawnTree(Vector3 spawnPosition)
+    {
+        GameObject go = Instantiate((GameObject)Resources.Load("Prefabs/Tree"));
+        go.transform.position = new Vector3(spawnPosition.x, spawnPosition.y, spawnPosition.z);
+    }
+    private void SpawnCage(Vector3 spawnPosition)
+    {
+        GameObject go = Instantiate((GameObject)Resources.Load("Prefabs/Cage"));
+        go.transform.position = new Vector3(spawnPosition.x, spawnPosition.y, spawnPosition.z);
+    }
+    private void SpawnPrisoner(Vector3 spawnPosition)
+    {
+        GameObject go = Instantiate((GameObject)Resources.Load(string.Format("Prefabs/Prisoners/Prisoner{0}", Random.Range(1, 11).ToString())));
+        go.transform.position = new Vector3(spawnPosition.x, spawnPosition.y, spawnPosition.z);
+    }
+}
