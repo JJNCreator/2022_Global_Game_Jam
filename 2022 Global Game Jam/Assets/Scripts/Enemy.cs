ad0de8a02a0ce9629c72a3a144a806f40ebd83ce
Jake Bayer <jakebayerct@gmail.com>
2022-01-29 14:49:14 -0500

    Update
    - Added enemy AI
    - Added more tree spawn points
    - Added more cage spawn points
    - Added more prisoner spawn points
    - Added more enemy spawn points

--- 2022 Global Game Jam/Assets/Scripts/Enemy.cs
+++ 2022 Global Game Jam/Assets/Scripts/Enemy.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform target;
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    public bool canMove = false;
+
+    private Transform transformCache;
+
+    private void Awake()
+    {
+        transformCache = transform;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(canMove)
+        {
+            transformCache.rotation = Quaternion.Slerp(transformCache.rotation, Quaternion.LookRotation(target.position - transformCache.position), rotateSpeed * Time.deltaTime);
+            transformCache.position += transformCache.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            canMove = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            canMove = false;
+        }
     }
 }
--- 2022 Global Game Jam/Assets/Scripts/GameManager.cs
+++ 2022 Global Game Jam/Assets/Scripts/GameManager.cs
@@ -48,6 +48,8 @@ public class GameManager : MonoBehaviour
                     {
                         SpawnEnemy(findSpawnPoints[i].transform.position);
                     }
+                    //SpawnEnemy(findSpawnPoints[i].transform.position);
+
                     break;
             }
         }
