5997f7d21617fe0a9febaedd8306b2f70b4f57fb
Jake Bayer <jakebayerct@gmail.com>
2022-01-29 10:29:31 -0500

    Set up player movement

---
+++ 2022 Global Game Jam/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerCharacter : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    private Transform transformCache;
+    private CharacterController cController;
+    private Vector3 moveDirection;
+
+    private void Awake()
+    {
+        cController = GetComponent<CharacterController>();
+        transformCache = transform;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    private void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            cController.Move(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+}
