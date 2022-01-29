283dd1fd1f563e73eb719ba62eb7d8ca516a8742
toaddoesnot <laiskusn7@gmail.com>
2022-01-29 14:24:34 -0500

    healthBar added
    added a health UI with a slider and a Health script that can be attached to the player

---
+++ 2022 Global Game Jam/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+
+    public healthBar healthBar;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (something)
+        // { TakeDamage(1); }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
