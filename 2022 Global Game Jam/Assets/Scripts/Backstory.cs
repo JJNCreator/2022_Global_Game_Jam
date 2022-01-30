8dd18e1853a18a44d5315f6b429bfbf18aec3364
Jake Bayer <jakebayerct@gmail.com>
2022-01-30 14:47:22 -0500

    Set up backstory scene

---
+++ 2022 Global Game Jam/Assets/Scripts/Backstory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Backstory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AnimationSequence());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator AnimationSequence()
+    {
+        yield return new WaitForSeconds(13f);
+
+        AsyncOperation aOperation = SceneManager.LoadSceneAsync(2);
+
+        while(!aOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
