a6cb69a3e851283f3802f44ca1be604afab3a796
Jake Bayer <jakebayerct@gmail.com>
2022-01-30 12:24:07 -0500

    Update
    - Improved cage trigger area
    - Added code for loading main menu after the game ends

--- 2022 Global Game Jam/Assets/Scripts/GameEnd.cs
+++ 2022 Global Game Jam/Assets/Scripts/GameEnd.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnd : MonoBehaviour
 {
     public GameObject win;
     public GameObject lose;
     public Health healthScript;
+
+    private bool isLoadingMainMenu = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +25,32 @@ public class GameEnd : MonoBehaviour
        if (GameManager.Instance.cagesFreedCount >= GameManager.Instance.cagesToBeFreed)
        {
             win.SetActive(true);
+            if(!isLoadingMainMenu)
+            {
+                StartCoroutine(LoadMainMenu());
+            }
        }
 
         if (healthScript.Dead)
         {
             lose.SetActive(true);
             Time.timeScale = 0;
+            if (!isLoadingMainMenu)
+            {
+                StartCoroutine(LoadMainMenu());
+            }
+        }
+    }
+    private IEnumerator LoadMainMenu()
+    {
+        isLoadingMainMenu = true;
+        yield return new WaitForSecondsRealtime(4f);
+
+        AsyncOperation aOperation = SceneManager.LoadSceneAsync(0);
+
+        while(!aOperation.isDone)
+        {
+            yield return null;
         }
     }
 }
