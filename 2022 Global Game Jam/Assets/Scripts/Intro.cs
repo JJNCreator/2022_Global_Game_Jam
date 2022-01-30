7339e2b638f803f5f0203d1798aa9622baff37c3
toaddoesnot <laiskusn7@gmail.com>
2022-01-30 14:08:12 -0500

    added the art but its not animated

---
+++ 2022 Global Game Jam/Assets/Scripts/Intro.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+    public GameObject art;
+    public GameObject story;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StoryStarts();
+    }
+
+
+    public void StoryStarts()
+    {
+       // story.SetActive(true);
+    }
+
+    public void StoryEnds()
+    {
+        story.SetActive(false);
+    }
+
+}
---
+++ 2022 Global Game Jam/Assets/Scripts/transformArt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformArt : MonoBehaviour
+{
+    public Vector3 target;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        transform.Translate (target * speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
