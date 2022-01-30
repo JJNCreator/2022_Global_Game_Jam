e29be8f2d7261c2d2695660ef35090846099eae7
Jake Bayer <jakebayerct@gmail.com>
2022-01-29 22:13:55 -0500

    Fix for small enemy AI collision bug
    Hopefully

--- 2022 Global Game Jam/Assets/Scripts/Enemy.cs
+++ 2022 Global Game Jam/Assets/Scripts/Enemy.cs
@@ -11,10 +11,12 @@ public class Enemy : MonoBehaviour
     public bool canMove = false;
 
     private Transform transformCache;
+    private Rigidbody rBody;
 
     private void Awake()
     {
         transformCache = transform;
+        rBody = GetComponent<Rigidbody>();
     }
     // Start is called before the first frame update
     void Start()
@@ -25,7 +27,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(canMove)
+        /*if(canMove)
         {
             //Debug.Log("Enemy:Update() - distance between enemy and player: " + Vector3.Distance(transformCache.position, target.position));
 
@@ -33,6 +35,19 @@ public class Enemy : MonoBehaviour
             {
                 transformCache.rotation = Quaternion.Slerp(transformCache.rotation, Quaternion.LookRotation(target.position - transformCache.position), rotateSpeed * Time.deltaTime);
                 transformCache.position += transformCache.forward * moveSpeed * Time.deltaTime;
+            }
+        }*/
+    }
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            //Debug.Log("Enemy:Update() - distance between enemy and player: " + Vector3.Distance(transformCache.position, target.position));
+
+            if (Vector3.Distance(transformCache.position, target.position) > 100f)
+            {
+                transformCache.rotation = Quaternion.Slerp(transformCache.rotation, Quaternion.LookRotation(target.position - transformCache.position), rotateSpeed * Time.fixedDeltaTime);
+                rBody.position += transformCache.forward * moveSpeed * Time.fixedDeltaTime;
             }
         }
     }
